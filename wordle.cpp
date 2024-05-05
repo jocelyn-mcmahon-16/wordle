c71991d5f284b9678b51cd3d59cf1ad4138a1674
Jocelyn McMahon <jocelyn.mcmahon@stonybrook.edu>
2024-05-04 20:26:09 -0400

    Updated and corrected code to properly output the guesses in game

--- wordle.cpp
+++ wordle.cpp
@@ -1,21 +1,58 @@
 #include <iostream>
-#include <string>
 #include <vector>
-
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <Windows.h>
 #include "wordle.H"
 
 int main() {
+    
+    const int word_length = 5;
+    const int NOT_MATCH = 0;
+    const int PARTIAL_MATCH = 1;
+    const int MATCH = 2;
+    const int num_guesses = 6;
+    int attempts = 6;
 
-int attempt = 0;
+    std::string guess;
+    int attempt = 0;
 
-    chooseSolution(5);
-    startGame();
-    while (guess != solution) {
-        if (attempt <= attempts) {
-            checkGuess();
-            attempt += 1;
+    std::vector<std::string> tries(attempts);    
+    std::vector<std::vector<int>> matches(attempts, std::vector<int>(word_length));
+
+
+    std::string solution = chooseSolution();
+    raiseCase(solution);
+    std::cout << solution << std::endl;
+
+
+    startGame(tries, matches, attempt);
+    std::cout << "Enter your guess (word length must be " + std::to_string(word_length) + ") or type Q to quit: ";
+
+
+    while (attempt < attempts) {
+      
+        do {
+            std::getline(std::cin, guess);
+            raiseCase(guess);
+        } while (guess != "Q" && !isValid(guess));
+        
+        if (guess == "Q") {
+            std::cout << "Quit game" << std::endl;
+            break;
+        }
+        tries[attempt] = guess;
+        checkGuess(matches, attempt, solution, guess);
+        printGuesses(tries, matches, attempt);
+        attempt++;
+        if (isAllMatch(solution, guess)) {
+            std::cout << "You found the word! :D" << std::endl;
+            checkAttempts(attempt, solution, guess);
+            break;
+        } else if (attempt == attempts) {
+            std::cout << "You didn't find the word... :'(" << std::endl;
+            checkAttempts(attempt, solution, guess);
         }
     }
 }
-
-// could maybe use the diff check to see if the words match?
